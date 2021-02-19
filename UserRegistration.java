442658bba1b38c72fd1fe2ee702bbf202bbc579d
InduLekha98 <rindulekha09@gmail.com>
2021-02-20 00:31:55 +0530

    refactored:Added first name and last name pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,14 +3,17 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	static Scanner sc = new Scanner(System.in);
-	public static void validation(String firstName) {
-		// First Name comes with restriction such as first letter should be upper case and with minimum 3 characters
+	public static void validation(String firstName,String lastName) {
+		// First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
 		System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
+		System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
 		System.out.println("Enter a valid first name");
 		String firstName = sc.nextLine();
-		validation(firstName);
+		System.out.println("Enter a valid last name");
+		String lastName = sc.nextLine();
+		validation(firstName, lastName);
 	}
 }
