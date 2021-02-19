7140ad0d29daaf4799db7ba2a62d2568553a97cc
InduLekha98 <rindulekha09@gmail.com>
2021-02-20 01:32:11 +0530

    refactored:refactored email pattern to satisfy sample TLD emails

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,7 @@ public class UserRegistration {
 		// First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
 		System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
 		System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
-	//  abc.xyz@bl.co.in is valid with abc bl and co are mandatory 2 optional(xyz,in) with precise @ and . positions 
+	        //  abc.xyz@bl.co.in is valid with abc bl and co are mandatory 2 optional(xyz,in) with precise @ and . positions 
 		System.out.println("E-Mail: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]{1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
 		// 91 followed by space followed by 10 digit number
 		System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9][0-9]{9}$", mobile));
