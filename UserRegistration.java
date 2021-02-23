9c57aa6019233b12f909d1b893615ad66226895f
InduLekha98 <rindulekha09@gmail.com>
2021-02-24 01:26:49 +0530

    Added:Added junit test file

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,33 +1,39 @@
+package com.UserRegistration;
+
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
-    public boolean isFirstNamevalid(String firstName) {
+    public boolean FirstNameIsValid(String firstName) {
         return Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName);
     }
-    public boolean isLastNameValid(String lastName) {
+
+    public boolean LastNameIsValid(String lastName) {
         return Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName);
     }
-    public boolean isEmailValid(String email) {
+
+    public boolean EmailIsValid(String email) {
         return Pattern.matches("^[a-zA-Z0-9]{3,}([\\.\\+\\-]?[a-zA-Z0-9]{3,})?[@][A-Za-z0-9]{1,}[.][A-Za-z]{2,4}[,]?([.][A-Za-z]{2,4}[.]?)?$", email);
     }
-    public boolean isMobileNumberValid(String mobile) {
+
+    public boolean MobileNumberIsValid(String mobile) {
         return Pattern.matches("^[9][1][\\s][6-9][0-9]{9}$", mobile);
     }
-    public boolean isPasswordValid(String password) {
+
+    public boolean PasswordIsValid(String password) {
         return Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$", password);
     }
+
     public static void main(String[] args) {
         String firstName = "Indu";
         String lastName = "Lekha";
-        String email = "abc.xyz@gmail.com";
-        String mobile = "91 7981118114";
-        String password = "Asjgdf45@fj";
-        UserRegistration person = new UserRegistration();
-        person.isFirstNamevalid(firstName);
-        person.isLastNameValid(lastName);
-        person.isEmailValid(email);
-        person.isMobileNumberValid(mobile);
-        person.isPasswordValid(password);
+        String email = "rindu@gmail.com";
+        String mobile = "91 7981234564";
+        String password = "ilrt@123";
+        UserRegistration individual = new UserRegistration();
+        individual.FirstNameIsValid(firstName);
+        individual.LastNameIsValid(lastName);
+        individual.EmailIsValid(email);
+        individual.MobileNumberIsValid(mobile);
+        individual.PasswordIsValid(password);
     }
 }
